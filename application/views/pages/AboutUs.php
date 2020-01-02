9a7c9165650ad70333d328eef98e1a6fc67203c1
SasiniSithara <sasini.2016143@iit.ac.lk>
2020-01-03 03:59:23 +0530

    aboutus

--- application/views/pages/AboutUs.php
+++ application/views/pages/AboutUs.php
@@ -164,15 +164,86 @@
 			margin-left: 4%;
 			margin-top: 1%;
 		}
+
+
+		/* about info  */
+		a {
+			color: #0254EB
+		}
+
+		a:visited {
+			color: #0254EB
+		}
+
+		a.morelink {
+			text-decoration: none;
+			outline: none;
+		}
+
+		.morecontent span {
+			display: none;
+		}
+
+		.comment {
+			width: 1000px;
+			background-color: #f0f0f0;
+			margin: 10px;
+		}
 	</style>
+
+	<script src="<?php echo base_url(); ?>assets/script/common.js"></script>
 	<!-- theam config -->
 	<!-- <link rel="stylesheet" href="../assets/themes/theam1/theam_d.css" />
 	<link rel="stylesheet" href="../assets/themes/theam1/jquery.mobile.icons.min.css" /> -->
 	<!-- <link rel="stylesheet" href="../assets/themes/theam2/themes/team_e_orange.css" />
 	<link rel="stylesheet" href="../assets/themes/theam2/themes/jquery.mobile.icons.min.css" /> -->
 
+	<script>
+		$(document).ready(function () {
+			var showChar = 400;
+			var ellipsestext = "...";
+			var moretext = "more";
+			var lesstext = "less";
+			$('.more').each(function () {
+				var content = $(this).html();
+
+				if (content.length > showChar) {
+
+					var c = content.substr(0, showChar);
+					var h = content.substr(showChar - 1, content.length - showChar);
+
+					var html = c + '<span class="moreellipses">' + ellipsestext + '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+
+					$(this).html(html);
+				}
+
+			});
+
+			$(".morelink").click(function () {
+				if ($(this).hasClass("less")) {
+					$(this).removeClass("less");
+					$(this).html(moretext);
+				} else {
+					$(this).addClass("less");
+					$(this).html(lesstext);
+				}
+				$(this).parent().prev().toggle();
+				$(this).prev().toggle();
+				return false;
+			});
+		});
+
+
+	</script>
+
+	<link href="http://www.jqueryscript.net/css/jquerysctipttop.css" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/jquery.feedback.css" type="text/css" />
+
+
 	<title>Aboutus page</title>
 </head>
+
+
 
 <body>
 <div data-role="page" id="demo-page" class="aboutus-page" data-url="demo-page">
@@ -231,11 +302,47 @@
 
 	</div>
 
-	<!-- green lable -->
 	<div data-role="header">
-		<a data-rel="back" class="ui-btn ui-shadow ui-corner-all ui-icon-arrow-l ui-btn-icon-notext" rel="external">Back</a>
-		<label for="title" class="title" >PRODUCT DETAIL</label>
+		<a data-rel="back" class="ui-btn ui-shadow ui-corner-all ui-icon-arrow-l ui-btn-icon-notext"
+		   rel="external">Back</a>
+		<label for="title" class="title">ABOUT US</label>
 		<!-- <a href="#" data-icon="check">Save</a> -->
+	</div>
+
+	<!-- green lable -->
+<!--	<div data-role="header">-->
+<!--		<a data-rel="back" class="ui-btn ui-shadow ui-corner-all ui-icon-arrow-l ui-btn-icon-notext" rel="external">Back</a>-->
+<!--		<label for="title" class="title" >ABOUT US</label>-->
+
+<!--	</div>-->
+
+	<div>
+		<h4 align="center">-Our Mission-</h4>
+		<h6 align="center">To provide the most actionable app store data.</h6>
+
+		<h5 align="center">About</h5>
+
+
+
+	</div>
+	<div class="comment more" style="width: 100%;">
+		Welcome to My Market, your number one source for all things product, ie: bevagers, vegetables,
+		fruits, etc.
+		We're dedicated to giving you the very best of this market products, with a focus on dependability,
+		customer service and uniqueness.
+		Founded in 2019 by Mr. M.L Perera, My Market has come a long way from its beginnings in a home
+		office. When Mr. M.L Perera first started out, his passion for helping other parents be more
+		eco-friendly,
+		providing the best equipment for his fellow musicians drove him to do intense research, quit her day
+		job, and gave him the impetus to turn hard work and inspiration into to a booming online store.
+		We now serve customers all over the US, the world, the Austin area, and are thrilled to be a part of
+		the eco-friendly wing of the baked goods industry.
+
+		We hope you enjoy our products as much as we enjoy offering them to you. If you have any questions
+		or comments, please don't hesitate to contact us.
+
+		Sincerely,
+		Sasini Anuthara, Co Founder
 	</div>
 
 
@@ -364,8 +471,52 @@
 		</div>
 	</div><!-- /rwd-example -->
 
+	<div align="center" style="background-color: #30343D ;">
+		<hr></hr>
+		<h5 style="font-family:sans-serif ;">Address</h5>
+		<h6  >No:01, Queen Rd, Colombo. </h6>
+		<br></br>
+		<h5 style="font-family:sans-serif ;">Latest Update</h5>
+		<h6>We added some location reveiws |2019 Feb|</h6>
+		<br></br>
+		<h5 style="font-family:sans-serif ;">Contact us</h5>
+
+		<!-- <button class="ui-btn ui-shadow ui-corner-all ui-btn-icon-left ui-icon-mail">mail</button> -->
+		<a href="#popupLogin" rel="popup" data-position-to="window" class="ui-btn ui-shadow ui-corner-all ui-icon-mail ui-btn-icon-notext" data-transition="pop">mail</a>
+		<br></br>
+
+		<div data-role="popup" id="popupLogin" data-theme="a" class="ui-corner-all">
+			    <form>
+				        <div style="padding:10px 20px;">
+					            <h3>Please sign in</h3>
+					<!--				            <label for="un" class="ui-hidden-accessible">Username:</label>-->
+					<!--				            <input type="text" name="user" id="un" value="" placeholder="username" data-theme="a">-->
+					<!--				            <label for="pw" class="ui-hidden-accessible">Password:</label>-->
+					<!--				            <input type="password" name="pass" id="pw" value="" placeholder="password" data-theme="a">-->
+					<!--				            <button type="submit" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check">Sign in</button>-->
+					        </div>
+				    </form>
+		</div>
+
+		<form id="frmBooking" name="bookingForm" method="post" action="_self"  >
+			<a data-role="button" class="cart-btn my-btn" id="customButton"
+			   style="background-color: #fe6311 !important; height: 40px  !important;  margin-bottom: 0px !important;  margin-top: 0px !important; color:white !important;">
+				PROCEED TO CHECKOUT
+			</a>
+		</form>
+
+	</div>
 
 
+
+<!--	<div id="feedback-anchor">-->
+<!---->
+<!--	</div>-->
+<!--	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>-->
+<!--	<script src="--><?php //echo base_url(); ?><!--assets/script/jquery.feedback.js"></script>-->
+<!--	<script type="text/javascript">-->
+<!--		Feedback.init();-->
+<!--	</script>-->
 
 
 
